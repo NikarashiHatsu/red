b717f5c6c3780cc94ce5e18d27ac386e1d1a5e9e
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-13 16:54:12 +0700

    fix: swapped locales

--- resources/lang/id/store.php
+++ resources/lang/id/store.php
@@ -1,14 +1,15 @@
+
 <?php
 
 return [
-    'greeting' => 'Hi, :name',
-    'further_notification' => 'Further information will be displayed when you\'ve requested the application creation.',
-    'submission_requirements' => 'Submission requirements:',
-    'pricing_plan' => 'Pricing plan',
-    'store_information' => 'Store information',
-    'application_information' => 'Application information',
-    'social_media_information' => 'Social media information',
-    'product_list' => 'Product list',
-    'layout_and_color_theme' => 'Layout and color theme',
-    'submit_application_creation' => 'Submit application creation',
+    'greeting' => 'Hai, :name',
+    'further_notification' => 'Informasi lebih lanjut akan ditampilkan saat Anda sudah mengajukan permintaan pembuatan aplikasi.',
+    'submission_requirements' => 'Persyaratan pengajuan:',
+    'pricing_plan' => 'Paket harga',
+    'store_information' => 'Informasi toko',
+    'application_information' => 'Informasi aplikasi',
+    'social_media_information' => 'Informasi sosial media',
+    'product_list' => 'Daftar produk',
+    'layout_and_color_theme' => 'Layout dan Tema Warna',
+    'submit_application_creation' => 'Ajukan pembuatan aplikasi',
 ];
