50150c000c4c03ba3a59fef9779a6920ab7d88ec
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-16 23:16:28 +0700

    chore: make livewire component of store information form

---
+++ app/Http/Livewire/StoreInformationForm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\FormOrder;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class StoreInformationForm extends Component
+{
+    use WithFileUploads;
+
+    public $store_banner_path;
+    public $store_logo_path;
+
+    public FormOrder $formOrder;
+
+    protected $rules = [
+        'formOrder.store_owner' => ['required', 'string'],
+        'formOrder.store_name' => ['required', 'string'],
+    ];
+
+    public function updatedStoreBannerPath()
+    {
+        $this->validate([
+            'store_banner_path' => ['required', 'image', 'max:4096'],
+        ]);
+    }
+
+    public function updatedStoreLogoPath()
+    {
+        $this->validate([
+            'store_logo_path' => ['required', 'image', 'max:4096'],
+        ]);
+    }
+
+    public function mount()
+    {
+        $this->formOrder = auth()->user()->formOrder()->firstOrCreate();
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        try {
+            if ($this->store_banner_path) {
+                if (Storage::exists($this->formOrder->store_banner_path)) Storage::delete($this->formOrder->store_banner_path);
+
+                $this->formOrder->store_banner_path = $this->store_banner_path->store('store_banners/' . $this->formOrder->id);
+            }
+
+            if ($this->store_logo_path) {
+                if (Storage::exists($this->formOrder->store_logo_path)) Storage::delete($this->formOrder->store_logo_path);
+
+                $this->formOrder->store_logo_path = $this->store_logo_path->store('store_logos/' . $this->formOrder->id);
+            }
+
+            $this->formOrder->update();
+        } catch (\Exception $e) {
+            return session()->flash('error', 'Informasi toko gagal disimpan: ' . $e->getMessage());
+        }
+
+        session()->flash('success', 'Informasi toko berhasil disimpan');
+    }
+
+    public function render()
+    {
+        return view('livewire.store-information-form');
+    }
+}
---
+++ resources/views/livewire/store-information-form.blade.php
@@ -0,0 +1,62 @@
+<form wire:submit.prevent="update" autocomplete="off">
+    @csrf
+    <div class="flex flex-col">
+        <label for="store_banner_path">Banner Toko <span class="text-red-500">*</span></label>
+        @error('store_banner_path') <span class="text-red-500 text-xs tracking-wider">{{ $message }}</span> @enderror
+        <div class="aspect-w-16 aspect-h-5 mt-2">
+            <label for="store_banner_path" class="transition duration-300 ease-in-out hover:bg-gray-50 w-full h-full rounded border border-gray-300 flex items-center justify-center cursor-pointer text-sm text-gray-500">
+                @if ($store_banner_path)
+                    <img src="{{ $store_banner_path->temporaryUrl() }}" class="w-full h-full rounded object-cover" />
+                @else
+                    @if ($formOrder->store_banner_path)
+                        <img src="{{ asset($formOrder->store_banner_path) }}" class="w-full h-full rounded object-cover" />
+                    @else
+                        16:5
+                    @endif
+                @endif
+            </label>
+        </div>
+        <input type="file" name="store_banner_path" wire:model="store_banner_path" id="store_banner_path" class="hidden" />
+    </div>
+
+    <div class="flex flex-col mt-4">
+        <label for="store_logo_path">Logo Toko <span class="text-red-500">*</span></label>
+        @error('store_logo_path') <span class="text-red-500 text-xs tracking-wider">{{ $message }}</span> @enderror
+        <div class="w-24">
+            <div class="aspect-w-1 aspect-h-1 mt-2">
+                <label for="store_logo_path" class="transition duration-300 ease-in-out hover:bg-gray-50 w-full h-full rounded-full border border-gray-300 flex items-center justify-center text-center cursor-pointer text-sm text-gray-500">
+                    @if ($store_logo_path)
+                        <img src="{{ $store_logo_path->temporaryUrl() }}" class="w-full h-full rounded-full object-cover" />
+                    @else
+                        @if ($formOrder->store_logo_path)
+                            <img src="{{ asset($formOrder->store_logo_path) }}" class="w-full h-full rounded-full object-cover" />
+                        @else
+                            16:5
+                        @endif
+                    @endif
+                </label>
+            </div>
+        </div>
+        <input type="file" name="store_logo_path" wire:model="store_logo_path" id="store_logo_path" class="hidden" />
+    </div>
+
+    <div class="flex flex-col mt-4">
+        <label for="store_owner">Nama Pemilik Toko <span class="text-red-500">*</span></label>
+        @error('formOrder.store_owner') <span class="text-red-500 text-xs tracking-wider">{{ $message }}</span> @enderror
+        <input type="text" name="store_owner" wire:model="formOrder.store_owner" id="store_owner" class="mt-2 rounded border-gray-300" required>
+    </div>
+
+    <div class="flex flex-col mt-4">
+        <label for="store_name">Nama Toko <span class="text-red-500">*</span></label>
+        @error('formOrder.store_name') <span class="text-red-500 text-xs tracking-wider">{{ $message }}</span> @enderror
+        <input type="text" name="store_name" wire:model="formOrder.store_name" id="store_name" class="mt-2 rounded border-gray-300" required>
+    </div>
+
+    <div class="flex justify-end mt-4">
+        <x-button>
+            Simpan
+        </x-button>
+    </div>
+
+    <x-success-and-error-swal />
+</form>
