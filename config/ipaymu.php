7533b287b315c0c8654f4e730939eb5e12819d55
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-18 10:33:17 +0700

    chore: add ipaymu

---
+++ config/ipaymu.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ipaymu_virtual_account' => env('IPAYMU_VIRTUAL_ACCOUNT'),
+    'ipaymu_api_key' => env('IPAYMU_API_KEY'),
+    'ipaymu_production' => env('IPAYMU_PRODUCTION'),
+];
