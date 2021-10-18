1ba5dd9f0688d1d54abf23a89703c2b9c1f1fc8a
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-18 18:36:45 +0700

    chore: hide the user's navigation after having a transaction

--- resources/views/layouts/navigation_user.blade.php
+++ resources/views/layouts/navigation_user.blade.php
@@ -5,17 +5,19 @@
         </x-nav-link>
     </div>
 
-    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-        <x-nav-link :href="route('store.pricing_plan.index')" :active="request()->routeIs('store.pricing_plan.index')">
-            {{ __('navigation.pricing_plan') }}
-        </x-nav-link>
-    </div>
-
-    @if (auth()->user()->form_order->pricing_plan_id)
+    @if (!auth()->user()->transaction)
         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <x-nav-link :href="route('store.form_order.index')" :active="request()->routeIs('store.form_order.index')">
-                {{ __('navigation.form_order') }}
+            <x-nav-link :href="route('store.pricing_plan.index')" :active="request()->routeIs('store.pricing_plan.index')">
+                {{ __('navigation.pricing_plan') }}
             </x-nav-link>
         </div>
+
+        @if (auth()->user()->form_order->pricing_plan_id)
+            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <x-nav-link :href="route('store.form_order.index')" :active="request()->routeIs('store.form_order.index')">
+                    {{ __('navigation.form_order') }}
+                </x-nav-link>
+            </div>
+        @endif
     @endif
 @endif
