152d51ed6bdf2dbd7a849ec60422ee3ed9413a35
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-23 02:06:58 +0700

    chore: remove the return all

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/store/notify', function() {
-    return response()->json(request()->all());
     try {
         FormOrder::where('sid', request()->sid)->update([
             'is_requested' => 1,
