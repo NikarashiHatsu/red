eeebe86a73bbccbfffe9d4f462ddb49bf6e343f4
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-22 17:59:36 +0000

    notify pembelian ipaymu

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/store/notify', function() {
+    return response()->json(request()->all());
     try {
         FormOrder::where('sid', request()->sid)->update([
             'is_requested' => 1,
