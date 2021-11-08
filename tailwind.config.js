0dc575f185155c76e5c5a593f43c8a9d1f1e508b
NikarashiHatsu <yourlovelydev@gmail.com>
2021-11-08 17:59:41 +0700

    chore: add @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,5 +62,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/typography'),
     ],
 };
