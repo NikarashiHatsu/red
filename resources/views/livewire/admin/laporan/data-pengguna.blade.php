31e01153e1cff69318b37ba1d635db5e0038cd52
NikarashiHatsu <yourlovelydev@gmail.com>
2021-11-12 22:14:45 +0700

    style: apply card to data-pengguna

--- resources/views/livewire/admin/laporan/data-pengguna.blade.php
+++ resources/views/livewire/admin/laporan/data-pengguna.blade.php
@@ -5,12 +5,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <livewire:datatable
-                model="\App\Models\User"
-                include="id, name|Nama, email"
-                hide="role, email_verified_at, created_at, updated_at"
-                exportable
-            />
+            <x-card class="p-4">
+                <livewire:datatable
+                    model="\App\Models\User"
+                    include="id, name|Nama, email"
+                    hide="role, email_verified_at, created_at, updated_at"
+                    searchable="name, email"
+                    exportable
+                />
+            </x-card>
         </div>
     </div>
 </div>
