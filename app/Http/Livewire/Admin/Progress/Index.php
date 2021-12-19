64a728da717552688b3a9915433c5cdc8ddad38e
NikarashiHatsu <yourlovelydev@gmail.com>
2021-12-19 11:30:57 +0700

    fix: pengajuan error

--- app/Http/Livewire/Admin/Progress/Index.php
+++ app/Http/Livewire/Admin/Progress/Index.php
@@ -12,15 +12,23 @@ class Index extends Component
 
     public function render()
     {
+        $incomplete_progress = Progress::query()
+            ->where('is_apk_created', null)
+            ->orWhere('is_published_on_google_play', null)
+            ->orWhere('google_play_url', null)
+            ->paginate(10);
+
+        $completed_progress = Progress::query()
+            ->with('user', 'user.form_order')
+            ->whereHas('user.form_order')
+            ->where('is_apk_created', true)
+            ->where('is_published_on_google_play', true)
+            ->where('google_play_url', '!=', null)
+            ->paginate(10);
+
         return view('livewire.admin.progress.index', [
-            'incomplete_progresses' => Progress::where('is_apk_created', null)
-                ->orWhere('is_published_on_google_play', null)
-                ->orWhere('google_play_url', null)
-                ->paginate(10),
-            'completed_progresses' => Progress::where('is_apk_created', true)
-                ->where('is_published_on_google_play', true)
-                ->where('google_play_url', '!=', null)
-                ->paginate(),
+            'incomplete_progresses' => $incomplete_progress,
+            'completed_progresses' => $completed_progress,
         ]);
     }
 }
