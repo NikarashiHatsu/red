021d48f4c54211b4110c0c30792b15c8eaf4cecf
NikarashiHatsu <yourlovelydev@gmail.com>
2021-12-20 16:16:56 +0700

    chore: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    mode: 'jit',
-
     important: true,
 
     content: [
@@ -63,5 +61,6 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/typography'),
+        require('daisyui'),
     ],
 };
